efe13033f184a91795d1f020b5afd40dd2264fff
Stacie <sccallahan@protonmail.com>
2023-07-30 07:43:46 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Reads a text file and returns the list
+    of to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Writes the list of to-do items to the text file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
